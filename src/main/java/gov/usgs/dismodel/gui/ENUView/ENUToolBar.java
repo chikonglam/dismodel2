5d79453d346101fcbcfbedc50547f04ae4fa863d
chikonglam <chikonglam@users.noreply.github.com>
2011-04-10 23:10:32 -0700

    Tied in solve and cross val buttons

--- src/main/java/gov/usgs/dismodel/gui/ENUView/ENUToolBar.java
+++ src/main/java/gov/usgs/dismodel/gui/ENUView/ENUToolBar.java
@@ -1,16 +1,24 @@
 package gov.usgs.dismodel.gui.ENUView;
 
+import gov.usgs.dismodel.SimulationDataModel;
+import gov.usgs.dismodel.calc.inversion.CrossValidator2;
+import gov.usgs.dismodel.calc.inversion.DistSlipSolveWorker;
+import gov.usgs.dismodel.calc.inversion.SASolveWorker;
 import gov.usgs.dismodel.gui.components.AllGUIVars;
 import gov.usgs.dismodel.gui.components.IconButton;
+import gov.usgs.dismodel.gui.events.GuiUpdateRequestListener;
 
+import javax.swing.JOptionPane;
 import javax.swing.JToolBar;
 
 public class ENUToolBar extends JToolBar{
     protected AllGUIVars allGuiVars;
+    protected SimulationDataModel simModel;
 
     public ENUToolBar(AllGUIVars allGuiVars) {
         super();
         this.allGuiVars = allGuiVars;
+        this.simModel = allGuiVars.getSimModel();
         
         this.add( new SnapToXyButton("Snap to X Y", "/gov/usgs/dismodel/resources/cartesian.png") );
         this.add( new DragXyButton("Drag X Y space", "/gov/usgs/dismodel/resources/hand.png") );
@@ -75,13 +83,19 @@ public class ENUToolBar extends JToolBar{
 
         @Override
         protected void buttonClicked() {
-            // TODO Auto-generated method stub
+            if (!simModel.isDistributedFaultProblem()) {
+                SASolveWorker saSolver = new SASolveWorker(allGuiVars);
+                saSolver.execute();
+            } else {
+                DistSlipSolveWorker dsSolver = new DistSlipSolveWorker(allGuiVars);
+                dsSolver.execute();
+            }
             
         }
         
     }
     
-    private class CrossValButton extends IconButton{
+    private class CrossValButton extends IconButton implements GuiUpdateRequestListener{
 
         public CrossValButton(String toolTip, String IconLocation) {
             super(toolTip, IconLocation);
@@ -90,6 +104,20 @@ public class ENUToolBar extends JToolBar{
 
         @Override
         protected void buttonClicked() {
+            CrossValidator2 cvSolver = new CrossValidator2(simModel, simModel.getOrigin());
+            
+            //TODO multithread this
+            double minCvssGam = cvSolver.calculate();
+            JOptionPane.showMessageDialog(allGuiVars.getMainFrame(),
+                    "CVSS is min when Gamma is: " + minCvssGam,
+                    "Cross Validation Result",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+            
+        }
+
+        @Override
+        public void guiUpdateAfterStateChange() {
             // TODO Auto-generated method stub
             
         }
